3534a540f6cac424c2da02a8954d67b726d7e456
ryotagh <ryota.fukada@gmail.com>
2026-05-17 21:52:49 -0700

    Update chat.js

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -8,6 +8,10 @@ const API_KEYS = [
   process.env.GEMINI_API_KEY_3,
   process.env.GEMINI_API_KEY_4,
   process.env.GEMINI_API_KEY_5,
+  process.env.GEMINI_API_KEY_6,
+  process.env.GEMINI_API_KEY_7,
+  process.env.GEMINI_API_KEY_8,
+  process.env.GEMINI_API_KEY_9,
 ].filter(Boolean);
 let keyIndex = 0;
 
